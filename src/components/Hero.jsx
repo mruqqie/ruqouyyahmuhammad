8607cb694e94cfe2e3959e6c05b08592c291f92e
mruqqie <mruqqie@gmail.com>
2023-11-21 00:13:33 +0100

    modified:   src/components/Hero.jsx 	modified:   src/components/canvas/Computers.jsx 	modified:   src/styles.js

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
 	return (
 		<section className="relative w-full h-screen">
 			<div
-				className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl
+				className={`${styles.paddingX} absolute inset-0 top-[90px] max-w-7xl
 		mx-auto flex flex-row items-start gap-5`}
 			>
 				<div className="flex flex-col justify-center items-center mt-5">
@@ -15,19 +15,39 @@ const Hero = () => {
 				</div>
 				<div>
 					<h1 className={`${styles.heroHeadText} text-white`}>
-						Hi! I'm{" "}
+						Hi! I'm <br className="md:hidden"/>
 						<span className="text-purple-600">Ruqouyyah</span>.
 					</h1>
 					<p
-						className={`${styles.heroSubText} mt-2 text-white-100 w-[70%]`}
+						className={`${styles.heroSubText} mt-2 text-white-100 xs:w-[300px] sm:w-[90%] w-[219px]`}
 					>
 						I transform ideas into delightful websites with a dash
-						of creativity and coding magic, making the web more
-						engaging. Let's embark on a digital adventure together!
+						of creativity and coding magic. Let's embark on a digital adventure together!
 					</p>
 				</div>
 			</div>
 			<ComputersCanvas />
+			<div
+				className="absolute bottom-1 w-full flex justify-center
+				items-center "
+			>
+				<a href="#about">
+					<div
+						className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary
+						flex justify-center items-start p-2"
+					>
+						<motion.div
+							animate={{ y: [0, 24, 0] }}
+							transition={{
+								duration: 1.5,
+								repeat: Infinity,
+								repeatType: "loop",
+							}}
+							className="w-3 h-3 rounded-full bg-secondary mb-1"
+						/>
+					</div>
+				</a>
+			</div>
 		</section>
 	);
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,8 +3,27 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import Loader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile, isMdScreen, isXsScreen, isSmScreen }) => {
 	const computer = useGLTF("./desktop_pc/scene.gltf");
+	let scale = 0.65;
+	let position = [0, -2.9, -1.0];
+	if (isMobile) {
+		scale = 0.4;
+		position = [0, -3.26, -0.6];
+	}
+	if (isMdScreen) {
+		scale = 0.6;
+		position = [0, -2.56, -1.0];
+	}
+	if (isSmScreen) {
+		scale = 0.5
+		
+		position = [0, -3.15, -1.5];
+	}
+	if (isXsScreen) {
+		scale = 0.5;
+		position = [0, -3.15, -0.89];
+	}
 	return (
 		<mesh>
 			<hemisphereLight intensity={2} groundColor="black" />
@@ -16,15 +35,69 @@ const Computers = () => {
 			<pointLight intensity={2} />
 			<primitive
 				object={computer.scene}
-				scale={0.75}
-				position={[0, -3.35, -1.5]}
-				rotation={[-0.01, -0.2, -0.1]}
-			></primitive>
+				scale={scale}
+				position={position}
+				rotation={[-0.01, -0.2, -0.04]}
+			/>
 		</mesh>
 	);
 };
 
 const ComputersCanvas = () => {
+	const [isMobile, setIsMobile] = useState(false);
+	const [isMdScreen, setIsMdScreen] = useState(false);
+	const [isXsScreen, setIsXsScreen] = useState(false);
+	const [isSmScreen, setIsSmScreen] = useState(false)
+	useEffect(() => {
+		const mediaQuery = window.matchMedia("(max-width: 449.9px)");
+		setIsMobile(mediaQuery.matches);
+		const handleMediaQueryChange = (e) => {
+			setIsMobile(e.matches);
+		};
+		mediaQuery.addEventListener("change", handleMediaQueryChange);
+		return () => {
+			mediaQuery.removeEventListener("change", handleMediaQueryChange);
+		};
+	}, []);
+	useEffect(() => {
+		const mediaQuery = window.matchMedia(
+			"(min-width: 768px) and (max-width: 1024px)"
+		);
+		setIsMdScreen(mediaQuery.matches);
+		const handleMediaQueryChange = (e) => {
+			setIsMdScreen(e.matches);
+		};
+		mediaQuery.addEventListener("change", handleMediaQueryChange);
+		return () => {
+			mediaQuery.removeEventListener("change", handleMediaQueryChange);
+		};
+	}, []);
+	useEffect(() => {
+		const mediaQuery = window.matchMedia(
+			"(min-width: 450px) and (max-width: 639.9px)"
+		);
+		setIsXsScreen(mediaQuery.matches);
+		const handleMediaQueryChange = (e) => {
+			setIsXsScreen(e.matches);
+		};
+		mediaQuery.addEventListener("change", handleMediaQueryChange);
+		return () => {
+			mediaQuery.removeEventListener("change", handleMediaQueryChange);
+		};
+	}, []);
+	useEffect(() => {
+		const mediaQuery = window.matchMedia(
+			"(min-width: 640px) and (max-width: 767.9px)"
+		);
+		setIsSmScreen(mediaQuery.matches);
+		const handleMediaQueryChange = (e) => {
+			setIsSmScreen(e.matches);
+		};
+		mediaQuery.addEventListener("change", handleMediaQueryChange);
+		return () => {
+			mediaQuery.removeEventListener("change", handleMediaQueryChange);
+		};
+	}, []);
 	return (
 		<Canvas
 			frameloop="demand"
@@ -38,7 +111,12 @@ const ComputersCanvas = () => {
 					maxPolarAngle={Math.PI / 2}
 					minPolarAngle={Math.PI / 2}
 				/>
-				<Computers />
+				<Computers
+					isMobile={isMobile}
+					isMdScreen={isMdScreen}
+					isXsScreen={isXsScreen}
+					isSmScreen={isSmScreen}
+				/>
 			</Suspense>
 			<Preload all />
 		</Canvas>
