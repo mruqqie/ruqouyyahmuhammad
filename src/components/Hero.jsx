ac543bafea1cf7e2c703a1538ce143640eb2ab83
mruqqie <mruqqie@gmail.com>
2023-12-08 12:50:08 +0100

    modified:   src/components/Hero.jsx 	modified:   src/index.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,10 +29,10 @@ const Hero = () => {
 			</div>
 			{/* <ComputersCanvas /> */}{" "}
 			<img
-					src="https://camo.githubusercontent.com/854abab80b1a8e70f616b87dc8f9e6f3fe84acd0c697a417ec2acb7ed355a415/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f4e5a4741785678664f764f7a45686a51374d2f67697068792e676966"
-					alt="Animated GIF"
-					className="absolute w-[350px] xs:w-[60%] sm:w-[55%] md:w-[50%] lg:w-[40%] right-0 bottom-12 xs:bottom-0 object-cover"
-				/>
+				src="https://camo.githubusercontent.com/854abab80b1a8e70f616b87dc8f9e6f3fe84acd0c697a417ec2acb7ed355a415/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f4e5a4741785678664f764f7a45686a51374d2f67697068792e676966"
+				alt="Animated GIF"
+				className="absolute w-[350px] xs:w-[60%] sm:w-[55%] md:w-[50%] lg:w-[40%] right-0 bottom-12 xs:bottom-0 object-cover"
+			/>
 			<div
 				className="absolute bottom-1 w-full flex justify-center
 				items-center "
